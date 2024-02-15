c4c8e91a4dc3365331886b791a4a2bf1ddc94e68
Aisulu <aisrysbekv@gmail.com>
2024-02-15 09:33:38 -0600

    Added task Rectangle and RectangleClients in day12

---
+++ src/day12_oop_encapsulation/RectangleClients.java
@@ -0,0 +1,15 @@
+package day12_oop_encapsulation;
+
+public class RectangleClients {
+
+    public static void main(String[] args) {
+
+        Rectangle red = new Rectangle(4,6);
+        Rectangle blue = new Rectangle(7.8, 12.3);
+        Rectangle yellow = new Rectangle(3.6, 8.9);
+
+        System.out.println(red);
+        System.out.println(blue);
+        System.out.println(yellow);
+    }
+}
